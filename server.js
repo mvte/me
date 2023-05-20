35c8996a2c59cba3be94960ccf81e12c26d4f2a8
jan <57322982+mvte@users.noreply.github.com>
2023-05-20 02:55:08 -0400

    fix refresh error

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(express.static(path.join(__dirname, "build")));
 
 // This route serves the React app
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(port, () => console.log(`Server listening on port ${port}`));
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
-  
-
-app.listen(port, () => console.log(`Server listening on port ${port}`));
