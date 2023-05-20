d24cfda7976fe62b70ccf150d2c232be9f572084
jan <57322982+mvte@users.noreply.github.com>
2023-05-20 02:49:34 -0400

    fix forms

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ app.use(express.static(path.join(__dirname, "build")));
 
 // This route serves the React app
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+  
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
