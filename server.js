8958cf5c800514e65ed6289c94805a5947eb1594
mvte <janmarzan11@gmail.com>
2023-09-24 14:00:04 -0400

    ssl redirect

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+import sslRedirect from 'heroku-ssl-redirect';
 // eslint-disable-next-line no-unused-vars
 // const bodyParser = require('body-parser');
 const path = require("path");
 const app = express();
+
 const port = process.env.PORT || 9000;
 
 app.use(express.static(path.join(__dirname, "build")));
+app.use(sslRedirect());
 
 // This route serves the React app
 app.get('/*', (req, res) => {
