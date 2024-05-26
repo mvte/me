4cc33bc9d6c9d7e1f54fa5aec3ca9cb42188b60d
mvte <janmarzan11@gmail.com>
2024-05-26 01:04:45 -0400

    sidebar styles for mobile

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -6,49 +6,49 @@ import './index.scss';
 import Logo from '../../assets/jm_logo.png';
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={Logo} alt="logo"/>
-            <h1>jan</h1>
-        </Link>
-        <nav>
-            <NavLink exact="true" activeClassName="active" to='/'>
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeClassName="active" className="about-link" to='/about'>
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeClassName="active" className="career-link" to='/career'>
-                <FontAwesomeIcon icon={faBook} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeClassName="active" className="projects-link" to='/projects'>
-                <FontAwesomeIcon icon={faCode} color="#4d4d4e"/>
-            </NavLink>
-            <NavLink exact="true" activeClassName="active" className="contact-link" to='/contact'>
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
-            </NavLink>
-        </nav>
-        <ul> 
-            <li>
-                <a 
-                target="_blank" 
-                rel="noreferrer"
-                href="https://www.linkedin.com/in/janmzn/"
-                >
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
-                </a>
-            </li>
-            <li>
-                <a 
-                target="_blank" 
-                rel="noreferrer"
-                href="https://github.com/mvte"
-                >
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
-                </a>
-            </li>
-        </ul>
-    </div>
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={Logo} alt="logo"/>
+                <h1>jan</h1>
+            </Link>
+            <nav>
+                <NavLink exact="true" activeClassName="active" to='/'>
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeClassName="active" className="about-link" to='/about'>
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeClassName="active" className="career-link" to='/career'>
+                    <FontAwesomeIcon icon={faBook} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeClassName="active" className="projects-link" to='/projects'>
+                    <FontAwesomeIcon icon={faCode} color="#4d4d4e"/>
+                </NavLink>
+                <NavLink exact="true" activeClassName="active" className="contact-link" to='/contact'>
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e"/>
+                </NavLink>
+            </nav>
+            <ul> 
+                <li>
+                    <a 
+                    target="_blank" 
+                    rel="noreferrer"
+                    href="https://www.linkedin.com/in/janmzn/"
+                    >
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e"/>
+                    </a>
+                </li>
+                <li>
+                    <a 
+                    target="_blank" 
+                    rel="noreferrer"
+                    href="https://github.com/mvte"
+                    >
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                    </a>
+                </li>
+            </ul>
+        </div>
 )
 
-export default Sidebar
+export default Sidebar;
