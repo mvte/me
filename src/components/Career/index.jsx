04d0a6a93fa9290a0d456989373944141ed1f61e
jan <57322982+mvte@users.noreply.github.com>
2025-03-05 00:51:29 -0500

    add most recent career experiences

--- src/components/Career/index.jsx
+++ src/components/Career/index.jsx
@@ -25,18 +25,39 @@ const Career = () => {
                     </h1>
                     <div className="timeline">
                         <TimelineCard
+                            company="Bloomberg"
+                            location="New York, NY"
+                            position="Software Engineer"
+                            date="April 2025"
+                            description="???"
+                        />
+                        <TimelineCard
+                            company="Colgate-Palmolive"
+                            location="Piscataway, NJ"
+                            position="Application & Software Development Intern"
+                            date="September 2024 - December 2024"
+                            description="HR Applications"
+                        />
+                         <TimelineCard
+                            company="Hack4Impact"
+                            location="Rutgers Chapter"
+                            position="Backend Developer"
+                            date="September 2023 - December 2024"
+                            description="Backend (MERN)"
+                        />
+                        <TimelineCard
+                            company="Verizon"
+                            location="Basking Ridge, NJ"
+                            position="Software Engineer Intern"
+                            date="June 2024 - August 2024"
+                            description="AI Automation"
+                        />
+                        <TimelineCard
                             company="Colgate-Palmolive"
                             location="Piscataway, NJ"
                             position="Application & Software Development Intern"
                             date="January 2024 - May 2024"
                             description="Internal Dependencies"
-                        />
-                        <TimelineCard
-                            company="Hack4Impact"
-                            location="Rutgers Chapter"
-                            position="Backend Developer"
-                            date="September 2023 - Current"
-                            description="Backend (MERN)"
                         />
                         <TimelineCard
                             company="Verizon"
